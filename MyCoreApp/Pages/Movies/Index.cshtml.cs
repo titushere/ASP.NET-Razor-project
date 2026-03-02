6b92b0e8546324d7229d3d699273f4ab82cb8a81
Titus Chacko <107310515+titushere@users.noreply.github.com>
2026-03-03 00:34:57 +0530

    added search and filter

--- MyCoreApp/Pages/Movies/Index.cshtml.cs
+++ MyCoreApp/Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MyCoreApp.Data;
 using MyCoreApp.Models;
@@ -21,8 +22,33 @@ namespace MyCoreApp.Pages.Movies
 
         public IList<Movie> Movie { get;set; } = default!;
 
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+
+        public SelectList? Genres { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? MovieGenre { get; set; }
+
         public async Task OnGetAsync()
         {
+            // <snippet_search_linqQuery>
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+            var movies = from m in _context.Movie
+                         select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
+            if (!string.IsNullOrEmpty(MovieGenre))
+            {
+                movies = movies.Where(x => x.Genre == MovieGenre);
+            }
+            // <snippet_search_selectList>
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            // </snippet_search_selectList>
             Movie = await _context.Movie.ToListAsync();
         }
     }
