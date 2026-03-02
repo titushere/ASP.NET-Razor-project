7daace9981600f0cf8d96d44b1bf509ca64e391f
Titus Chacko <107310515+titushere@users.noreply.github.com>
2026-03-02 19:29:05 +0530

    seed the database

--- MyCoreApp/Program.cs
+++ MyCoreApp/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using MyCoreApp.Data;
+using MyCoreApp.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,6 +11,13 @@ builder.Services.AddDbContext<MyCoreAppContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("MyCoreAppContext") ?? throw new InvalidOperationException("Connection string 'MyCoreAppContext' not found.")));
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
